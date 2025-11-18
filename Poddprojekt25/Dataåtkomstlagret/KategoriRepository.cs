aeae423fb84b9ad806742279fb873c73e86b24e8
MinnaSeger <minna.seger@hotmail.com>
2025-11-18 11:09:15 +0100

    Tillägg kategori-klass

---
+++ Poddprojekt25/Dataåtkomstlagret/KategoriRepository.cs
@@ -0,0 +1,53 @@
+﻿using MongoDB.Driver;
+using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dataåtkomstlagret
+{
+    public class KategoriRepository
+    {
+        private readonly IMongoCollection<Kategori> kategoriKollektion;
+
+        public KategoriRepository()
+        {
+            var klient = new MongoClient("mongodb+srv://minnaseger_db_user:Hejsanhoppsan123!@cluster0.n1t8w3b.mongodb.net/?appName=Cluster0");
+            var databas = klient.GetDatabase("Poddprojekt25");
+            kategoriKollektion = databas.GetCollection<Kategori>("Kategorier");
+        }
+
+        // Hämta kategori med Id
+        public async Task<Kategori> HämtaMedIdAsync(string id)
+        {
+            var filter = Builders<Kategori>.Filter.Eq(k => k.Id, id);
+            return await kategoriKollektion.Find(filter).FirstOrDefaultAsync();
+        }
+
+        // Hämta alla kategorier
+        public async Task<List<Kategori>> HämtaAllaAsync()
+        {
+            var filter = Builders<Kategori>.Filter.Empty;
+            return await kategoriKollektion.Find(filter).ToListAsync();
+        }
+
+        // Lägg till ny kategori
+        public async Task LäggTillAsync(Kategori kategori)
+        {
+            await kategoriKollektion.InsertOneAsync(kategori);
+        }
+
+        // Uppdatera kategori
+        public async Task UppdateraAsync(Kategori uppdateradKategori, IClientSessionHandle session)
+        {
+            var filter = Builders<Kategori>.Filter.Eq(k => k.Id, uppdateradKategori.Id);
+            await kategoriKollektion.ReplaceOneAsync(session, filter, uppdateradKategori);
+        }
+
+        // Ta bort kategori med Id
+        public async Task TaBortAsync(string id)
+        {
+            var filter = Builders<Kategori>.Filter.Eq(k => k.Id, id);
+            await kategoriKollektion.DeleteOneAsync(filter);
+        }
+    }
+}
---
+++ Poddprojekt25/Models/Kategori.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System.Collections.Generic;
+
+namespace Models
+{
+    public class Kategori
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("Namn")]
+        public string? Namn { get; set; }
+
+        // Lista med podcast-ID:n som hör till denna kategori
+        [BsonElement("PodcastIds")]
+        public List<string>? PodcastIds { get; set; }
+    }
+}
